dca5ef411431896bd98f5e2ff90e8410cac5f010
jccl2 <jcclucas2013@gmail.com>
2025-07-20 16:06:52 -0300

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +25,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // Forçar HTTPS em todas as URLs geradas pela aplicação
+        if (env('APP_ENV') !== 'local') {
+            URL::forceScheme('https');
+        }
+
+        // Configurar comprimento padrão para colunas string
+        Schema::defaultStringLength(191);
     }
 }
